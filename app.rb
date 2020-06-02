650b59bedf3ba0a1c92fcb795f0f199773ef2649
Curtis Chadwell <laurencechadwell@gmail.com>
2020-06-02 05:39:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+      @pirate = Pirate.new(:pirate)
       # params[:pirate]
 
       params[:pirate][:ships].each do |details|
